e822f700ee2f0b8fe9cae00fcc9b6811b702d928
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-11-09 15:16:17 +0530

    Refactor database connection and enhance Person model with validation and CRUD operation

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   const MONGODB_URI =
-    "mongodb+srv://Nimas:great123@cluster0.tw9kkxo.mongodb.net/express";
+    "mongodb+srv://Nimas:great123@cluster0.tw9kkxo.mongodb.net/greatStack";
 
   await mongoose.connect(MONGODB_URI).then(() => {
     console.log("Database Connected");
--- index.js
+++ index.js
@@ -1,13 +1,50 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
+import { Person } from "./models/person.js";
 
 const app = express();
 const PORT = 3000;
 
 await connectDB();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello, express");
+});
+
+//saving data in MongoDB
+app.post("/person", async (req, res) => {
+  try {
+    const { email, name, age } = req.body;
+    const newPerson = new Person({
+      name,
+      age,
+      email,
+    });
+    await newPerson.save();
+    console.log(newPerson);
+    res.send("Person added");
+  } catch (error) {
+    res.send(error.message);
+  }
+});
+
+//Updating data in MongoDB
+app.put("/person", async (req, res) => {
+  const { id } = req.body;
+
+  const personData = await Person.findByIdAndUpdate(id, { age: "28" });
+  await personData.save();
+  console.log(personData);
+  res.send("Person Updated");
+});
+
+//Detele data in MongoDB
+app.delete("/person/:id", async (req, res) => {
+  const { id } = req.params;
+  await Person.findByIdAndDelete(id);
+  res.send("User Deleted");
 });
 
 app.listen(PORT, () => {
