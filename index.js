92da464093073c0d55cbd79080835612262cefc5
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-11-09 12:11:21 +0530

    mongoDB atlas setup

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = 3000;
+const MONGODB_URI =
+  "mongodb+srv://Nimas:great123@cluster0.tw9kkxo.mongodb.net/express";
+
+await mongoose.connect(MONGODB_URI).then(() => {
+  console.log("Database Connected");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello, express");
