6f0ab357966cee8e530202be610d1e4d176e24bf
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-11-09 16:44:56 +0530

    Add cookie-parser dependency and implement cookie handling in the Express app

--- index.js
+++ index.js
@@ -1,50 +1,21 @@
+import cookieParser from "cookie-parser";
 import express from "express";
-import { connectDB } from "./config/db.js";
-import { Person } from "./models/person.js";
 
 const app = express();
 const PORT = 3000;
-
-await connectDB();
-
-app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
+  res.cookie("name", "express-app", { maxAge: 360000 });
   res.send("Hello, express");
 });
-
-//saving data in MongoDB
-app.post("/person", async (req, res) => {
-  try {
-    const { email, name, age } = req.body;
-    const newPerson = new Person({
-      name,
-      age,
-      email,
-    });
-    await newPerson.save();
-    console.log(newPerson);
-    res.send("Person added");
-  } catch (error) {
-    res.send(error.message);
-  }
+app.get("/fetch", (req, res) => {
+  console.log(req.cookies);
+  res.send("API Called");
 });
-
-//Updating data in MongoDB
-app.put("/person", async (req, res) => {
-  const { id } = req.body;
-
-  const personData = await Person.findByIdAndUpdate(id, { age: "28" });
-  await personData.save();
-  console.log(personData);
-  res.send("Person Updated");
-});
-
-//Detele data in MongoDB
-app.delete("/person/:id", async (req, res) => {
-  const { id } = req.params;
-  await Person.findByIdAndDelete(id);
-  res.send("User Deleted");
+app.get("/remove-cookie", (req, res) => {
+  res.clearCookie("name");
+  res.send("Cookie cleared");
 });
 
 app.listen(PORT, () => {
