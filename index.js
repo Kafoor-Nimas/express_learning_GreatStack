71fcbe283ed2e236bff50154af01d65f2c8be96d
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-11-09 17:12:19 +0530

    session management

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 import cookieParser from "cookie-parser";
 import express from "express";
+import session from "express-session";
 
 const app = express();
 const PORT = 3000;
 app.use(cookieParser());
+app.use(
+  session({
+    secret: "sample-secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.get("/", (req, res) => {
-  res.cookie("name", "express-app", { maxAge: 360000 });
   res.send("Hello, express");
 });
-app.get("/fetch", (req, res) => {
-  console.log(req.cookies);
-  res.send("API Called");
+app.get("/visit", (req, res) => {
+  if (req.session.page_views) {
+    req.session.page_views++;
+    res.send(`You visited this page ${req.session.page_views} times`);
+  } else {
+    req.session.page_views = 1;
+    res.send("Welcome to this page for the first time!");
+  }
 });
-app.get("/remove-cookie", (req, res) => {
-  res.clearCookie("name");
-  res.send("Cookie cleared");
+
+app.get("/remove-visit", (req, res) => {
+  req.session.destroy();
+  res.send("Session Removed");
 });
 
 app.listen(PORT, () => {
