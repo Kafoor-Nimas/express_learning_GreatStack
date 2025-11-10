8c5bafcfb55c0170cba7f37133b2168a85d46f46
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-11-10 08:33:09 +0530

    Project Scaffolding & Global error handling

--- index.js
+++ index.js
@@ -7,35 +7,27 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello, express");
 });
-// Get all products
-app.get("/api/products", (req, res) => {
-  const products = [
-    { id: 1, name: "Laptop", price: 1000 },
-    { id: 2, name: "Mobile", price: 500 },
-  ];
-  res.status(200).json({ products });
-});
-// Get a single product
-app.get("/api/products/:id", (req, res) => {
-  const products = [
-    { id: 1, name: "Laptop", price: 1000 },
-    { id: 2, name: "Mobile", price: 500 },
-  ];
-
-  const product = products.find((p) => p.id === Number(req.params.id));
-
-  if (!product) {
-    return res.status(404).json({ message: "Product not found" });
+// Synchronous Error
+app.get("/sync-error", (req, res, next) => {
+  try {
+    throw new Error("Something went wrong!");
+  } catch (error) {
+    next(error);
   }
-  res.status(200).json(product);
+});
+//Asynchronous Error
+app.get("/async-error", async (req, res, next) => {
+  try {
+    await Promise.reject(new Error("Async error occured"));
+  } catch (error) {
+    next(error);
+  }
 });
 
-//Create a new product
-
-app.post("/api/products", (req, res) => {
-  const newProduct = req.body;
-  newProduct.id = Date.now();
-  res.status(201).json(newProduct);
+app.use((err, req, res, next) => {
+  console.error(err.message);
+  console.log(err.stack);
+  res.status(500).json({ message: err.message });
 });
 
 app.listen(PORT, () => {
